5fb689be6f9961ef8e3f56c5fa46a95c8a8d0091
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-24 16:01:59 +0100

    Add confirm password

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -31,10 +32,12 @@ class RegisterType extends AbstractType
                 'label' => 'Email',
                 'required' => true,
             ])
-            ->add('password', PasswordType::class, [
-                'attr' => ['class' => 'formulaire'],
-                'label' => 'Mot de passe',
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passes doivent correspondre.',
                 'required' => true,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
             ])
             ->add('Ajouter', SubmitType::class);
     }
