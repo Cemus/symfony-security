5d9b65f8ad1e92e8519361bef3e932f974eec420
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-25 15:10:27 +0100

    Add private pages

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,6 +10,11 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -18,10 +24,26 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+    }
+
+    //Méthode qui vérifie l'activation du compte
+    #[Route(path: '/verif', name: 'app_security_verif')]
+    public function verif(): Response
+    {
+        //Récupérer l'utilisateur connecté
+        $user = $this->getUser();
+        //Tester si le compte est activé
+        if ($user->isStatus() == false) {
+            //Redirection vers la route de déconnexion
+            return $this->redirectToRoute('app_logout');
+        }
+        //Redirection vers la page d'accueil
+        return $this->redirectToRoute('app_home');
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
